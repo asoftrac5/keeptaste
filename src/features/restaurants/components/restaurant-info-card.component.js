0e77e2bdc0c2fdd87c445856bfdd72294c90f4af
racshan <racshanraim555@gmail.com>
2021-12-28 23:38:11 +0530

    svg file

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,10 +2,37 @@ import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
-const Title = styled.Text`
-padding: 16px;
-color: red;
+const RestaurantCard = styled(Card)`
+  backgroundcolor: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  backgroundcolor: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled(Text)`
+font-family: ${(props) => props.theme.fonts.body};
+font-size: ${(props) => props.theme.fontSizes.caption}
+`;
+
+const Title = styled(Text)`
+font-family: ${(props) => props.theme.fonts.heading};
+font-size: ${(props) => props.theme.fontSizes.body}
+color: ${(props) => props.theme.colors.ui.success};
+`;
+
+const Info = styled.View`
+padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -18,17 +45,25 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily
+    isClosedTemporarily,
   } = restaurant;
-  return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
-    </Card>
-  )
-}
 
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-})
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={10} height={10} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
+
+const styles = StyleSheet.create({});
---
+++ src/infrastructure/theme/fonts.js
@@ -0,0 +1,23 @@
+export const fonts = {
+  body: "Lato_400Regular",
+  heading: "Lato_400Regular",
+  monospace: "Oswald_400Regular",
+};
+
+export const fontWeights = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};
+
+export const fontSizes = {
+  caption: "12px",
+  button: "14px",
+  body: "16px",
+  title: "12px",
+  h5: "25px",
+  h4: "34px",
+  h3: "45px",
+  h2: "56px",
+  h1: "112px",
+};
